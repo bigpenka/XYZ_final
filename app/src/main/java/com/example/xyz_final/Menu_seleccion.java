320fe16e8983bbb6bca385773d1d5a3514209de7
bigpenka <137832735+bigpenka@users.noreply.github.com>
2023-11-23 17:34:06 -0300

    Initial commit

---
+++ app/src/main/java/com/example/xyz_final/Menu_seleccion.java
@@ -0,0 +1,15 @@
+package com.example.xyz_final;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Menu_seleccion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista);
+    }
+
+}
